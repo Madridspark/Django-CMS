02808616c41512b36196c40f96a033ca53f245b0
Madridspark <1059494754@qq.com>
2017-01-23 14:46:14 +0800

    1.23@django_static

--- winter/urls.py
+++ winter/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from winter.view import hello
 
 urlpatterns = [
+    url(r'^$', hello),
     url(r'^admin/', admin.site.urls),
 ]
